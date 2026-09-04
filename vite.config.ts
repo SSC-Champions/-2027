85ae48392e2131e5a8aac8b886d591c9cdd61228
SSC-Champions <swamyschoolmaths@gmail.com>
2026-09-04 22:29:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // GitHub repository name
   base: '/-2027/',
-  plugins: [react()],
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
